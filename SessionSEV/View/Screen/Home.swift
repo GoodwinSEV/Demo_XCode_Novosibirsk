6c4d5960a429d0f7588ba5582c0b05df578112d5
GoodwinSEV <esavvina51@gmail.com>
2024-09-26 13:38:27 +0700

    Экран HOME

--- SessionSEV/View/Screen/Home.swift
+++ SessionSEV/View/Screen/Home.swift
@@ -7,11 +7,91 @@
 
 import SwiftUI
 
-struct Home: View {
-    var body: some View {
-        Text("Hello, World!")
+struct HomeView: View{
+    var body: some View{
+        VStack{
+            
+            Image(systemName: "1.circle")
+                .padding()
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+            Text("Home")
+        }
     }
 }
+
+
+struct WalletView: View {
+    var body: some View{
+        VStack{
+            Image(systemName: "")
+                .padding()
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+            Text("Wallet")
+        }
+    }
+}
+
+struct TrackView: View {
+    var body: some View{
+        VStack{
+            Image(systemName: "2.circle")
+                .padding()
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+            Text("Track")
+        }
+    }
+}
+
+struct ProfileView: View {
+    var body: some View{
+        VStack{
+            Image(systemName: "2.circle")
+                .padding()
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+            Text("Profile")
+        }
+    }
+}
+
+struct Home: View {
+    var body: some View {
+        TabView{
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            WalletView()
+                .tabItem {
+                    Image(systemName: "wallet-3")
+                    Text("Wallet")
+                }
+            TrackView()
+                .tabItem {
+                    Image(systemName: "track")
+                    Text("Track")
+                }
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "profile-circle")
+                    Text("Profile")
+                }
+            
+        }
+    }
+}
+
+
+
+//struct Home: View {
+//    var body: some View {
+//        Text("Hello, World!")
+//    }
+//}
 
 #Preview {
     Home()
